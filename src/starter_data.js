9a86330f0e2a7df356e53a0107a73b974aa055a4
EgimenoS <ergisan@gmail.com>
2020-08-06 21:10:39 +0200

    Commit Inicial. Creado el proyecto con create-react-app y apliado reset css

---
+++ src/starter_data.js
@@ -0,0 +1,35 @@
+const data = {
+  lists: [
+    {
+      id: "1",
+      title: "Tareas por hacer",
+      cards: [
+        {
+          id: "1",
+          title: "Cortarme el pelo",
+          description: "Ir a la peluquería a cortarme el pelo",
+        },
+        {
+          id: "2",
+          title: "Terminar de ver True Detective",
+          description: "Ver la 3ª temporada que tengo pendiente",
+        },
+      ],
+    },
+    {
+      id: "2",
+      title: "Completados",
+      cards: [
+        {
+          id: "1",
+          title: "Practicar React",
+          description: "Practicar React emulando trello",
+          label: "5 mins",
+          metadata: { sha: "be312a1" },
+        },
+      ],
+    },
+  ],
+};
+
+export default data;
